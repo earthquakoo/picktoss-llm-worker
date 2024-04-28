c5cb3f7f194f30d99fa92de82a5d38bc4b9f53e9
Earthquakoo <cream5343@gmail.com>
2024-04-28 17:27:32 +0900

    picktoss llm worker default settings

---
+++ src/app/quiz/mix_up.py
@@ -0,0 +1,127 @@
+from datetime import datetime
+
+from src.core.database.database_manager import DatabaseManager
+from src.core.s3.s3_client import S3Client
+from src.core.discord.discord_client import DiscordClient
+from src.core.llm.openai import OpenAIChatLLM
+from src.core.llm.exception import InvalidLLMJsonResponseError
+from src.core.enums.enum import LLMErrorType, SubscriptionPlanType, QuizQuestionNum, DocumentStatus, QuizType
+from src.core.llm.utils import fill_message_placeholders, load_prompt_messages
+
+def mix_up_worker(
+    s3_client: S3Client,
+    discord_client: DiscordClient, 
+    db_manager: DatabaseManager, 
+    chat_llm: OpenAIChatLLM,
+    s3_key: str,
+    db_pk: int,
+    subscription_plan: str
+    ):
+    bucket_obj = s3_client.get_object(key=s3_key)
+    content = bucket_obj.decode_content_str()
+    
+    # Generate Questions
+    CHUNK_SIZE = 1100
+    chunks: list[str] = []
+    for i in range(0, len(content), CHUNK_SIZE):
+        chunks.append(content[i : i + CHUNK_SIZE])
+    # dev & prod
+    without_placeholder_messages = load_prompt_messages("/var/task/src/core/llm/prompts/generate_mix_up_quiz.txt") 
+    # local
+    # without_placeholder_messages = load_prompt_messages("src/core/llm/prompts/generate_mix_up_quiz.txt")
+    free_plan_question_expose_count = 0
+    total_generated_question_count = 0
+
+    success_at_least_once = False
+    failed_at_least_once = False
+
+    prev_questions: list[str] = []
+    for chunk in chunks:
+        prev_question_str = '\n'.join([q for q in prev_questions])
+        messages = fill_message_placeholders(messages=without_placeholder_messages, placeholders={"note": chunk, "prev_questions": prev_question_str})
+        try:
+            resp_dict = chat_llm.predict_json(messages)
+        except InvalidLLMJsonResponseError as e:
+            discord_client.report_llm_error(
+                task="Question Generation",
+                error_type=LLMErrorType.INVALID_JSON_FORMAT,
+                document_content=chunk,
+                llm_response=e.llm_response,
+                error_message="LLM Response is not JSON-decodable",
+                info=f"* s3_key: `{s3_key}`\n* document_id: `{db_pk}`",
+            )
+            failed_at_least_once = True
+            continue
+        except Exception as e:
+            discord_client.report_llm_error(
+                task="Question Generation",
+                error_type=LLMErrorType.GENERAL,
+                document_content=chunk,
+                error_message="Failed to generate questions",
+                info=f"* s3_key: `{s3_key}`\n* document_id: `{db_pk}`",
+            )
+            failed_at_least_once = True
+            continue
+
+        try:
+            for q_set in resp_dict:
+                question, answer, explanation = q_set["question"], q_set["answer"], q_set["explanation"]
+                answer_count = 0
+
+                # To avoid duplication
+                prev_questions.append(question)
+                if len(prev_questions) == 6:
+                    prev_questions.pop(0)
+
+                total_generated_question_count += 1
+
+                if subscription_plan == SubscriptionPlanType.FREE.value:
+                    if free_plan_question_expose_count >= QuizQuestionNum.FREE_PLAN_QUIZ_QUESTION_NUM.value:
+                        delivered_count = 0
+                    else:
+                        delivered_count = 1
+                        free_plan_question_expose_count += 1
+                elif subscription_plan == SubscriptionPlanType.PRO.value:
+                    delivered_count = 1
+                else:
+                    raise ValueError("Wrong subscription plan type")
+                question_insert_query = "INSERT INTO quiz (question, answer, explanation, delivered_count, quiz_type, bookmark, answer_count, document_id, created_at, updated_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                timestamp = datetime.now()
+                db_manager.execute_query(question_insert_query, (question, answer, explanation, delivered_count, QuizType.MIX_UP.value, False, answer_count, db_pk, timestamp, timestamp))
+                db_manager.commit()
+
+        except Exception as e:
+            discord_client.report_llm_error(
+                task="Question Generation",
+                error_type=LLMErrorType.GENERAL,
+                document_content=chunk,
+                error_message=f"LLM Response is JSON decodable but does not have 'question' and 'answer' keys.\nresp_dict: {resp_dict}",
+                info=f"* s3_key: `{s3_key}`\n* document_id: `{db_pk}`",
+            )
+            failed_at_least_once = True
+            continue
+
+        success_at_least_once = True
+
+    # Failed at every single generation
+    if not success_at_least_once:
+        document_update_query = "UPDATE document SET status = %s WHERE id = %s"
+        db_manager.execute_query(document_update_query, (DocumentStatus.COMPLETELY_FAILED.value, db_pk))
+        db_manager.commit()
+        print("COMPLETELY_FAILED")
+        return
+
+    # Failed at least one chunk question generation
+    if failed_at_least_once:
+        document_update_query = "UPDATE document SET status = %s WHERE id = %s"
+        db_manager.execute_query(document_update_query, (DocumentStatus.PARTIAL_SUCCESS.value, db_pk))
+        db_manager.commit()
+        print("PARTIAL_SUCCESS")
+
+    else:  # ALL successful
+        document_update_query = "UPDATE document SET status = %s WHERE id = %s"
+        db_manager.execute_query(document_update_query, (DocumentStatus.PROCESSED.value, db_pk))
+        db_manager.commit()
+        print("PROCESSED")
+        
+    db_manager.close()
---
+++ src/worker/worker.py
@@ -0,0 +1,40 @@
+import os
+import json
+import logging
+from threading import Thread
+
+from src.core.s3.s3_client import S3Client
+from src.core.discord.discord_client import DiscordClient
+from src.core.llm.openai import OpenAIChatLLM
+from src.core.database.database_manager import DatabaseManager
+from src.app.keypoint.keypoint import keypoint_worker
+from src.app.quiz.mix_up import mix_up_worker
+from src.app.quiz.multiple_choice import multiple_choice_worker
+
+logging.basicConfig(level=logging.INFO)
+
+
+def handler(event, context):
+    event_info: str = event["Records"][0]["body"]
+    body: dict = json.loads(event_info)
+    if "s3_key" not in body or "db_pk" not in body or "subscription_plan" not in body:
+        raise ValueError(f"s3_key and db_pk and subscription_plan must be provided. event: {event}, context: {context}")
+    
+    s3_key = body["s3_key"]
+    db_pk = int(body["db_pk"])
+    subscription_plan = body["subscription_plan"]
+    # core client settings
+    s3_client = S3Client(access_key=os.environ["PICKTOSS_AWS_ACCESS_KEY"], secret_key=os.environ["PICKTOSS_AWS_SECRET_KEY"], region_name="us-east-1", bucket_name=os.environ["PICKTOSS_S3_BUCKET_NAME"])
+    discord_client = DiscordClient(bot_token=os.environ["PICKTOSS_DISCORD_BOT_TOKEN"], channel_id=os.environ["PICKTOSS_DISCORD_CHANNEL_ID"])
+    db_manager = DatabaseManager(host=os.environ["PICKTOSS_DB_HOST"], user=os.environ["PICKTOSS_DB_USER"], password=os.environ["PICKTOSS_DB_PASSWORD"], db=os.environ["PICKTOSS_DB_NAME"])
+    chat_llm = OpenAIChatLLM(api_key=os.environ["PICKTOSS_OPENAI_API_KEY"], model="gpt-3.5-turbo-0125")
+    
+    keypoint = Thread(target=keypoint_worker, args=(s3_client, discord_client, db_manager, chat_llm, s3_key, db_pk, subscription_plan))
+    mix_up = Thread(target=mix_up_worker, args=(s3_client, discord_client, db_manager, chat_llm, s3_key, db_pk, subscription_plan))
+    multiple_choice = Thread(target=multiple_choice_worker, args=(s3_client, discord_client, db_manager, chat_llm, s3_key, db_pk, subscription_plan))
+    
+    keypoint.start()
+    mix_up.start()
+    multiple_choice.start()
+
+    return {"statusCode": 200, "message": "hi"}
