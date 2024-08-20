f4818edeb9ec6e4ac86dd88727ff76d62e003ea3
earthquake <cream5343@gmail.com>
2024-08-20 15:01:22 +0900

    lambda test

--- worker/worker.py
+++ worker/worker.py
@@ -34,7 +34,7 @@ def handler(event, context):
     chat_llm = OpenAIChatLLM(api_key=os.environ["PICKTOSS_OPENAI_API_KEY"], model="gpt-4o-mini")
     db_manager = DatabaseManager(host=os.environ["PICKTOSS_DB_HOST"], user=os.environ["PICKTOSS_DB_USER"], password=os.environ["PICKTOSS_DB_PASSWORD"], db=os.environ["PICKTOSS_DB_NAME"])
 
-    time.sleep(60)
+    time.sleep(300)
 
     get_outbox_query = f"SELECT * FROM outbox WHERE document_id = {db_pk}"
     outbox = db_manager.execute_query(get_outbox_query)
